52cd3cd46b420661a259cfc8783d7c3334629cc2
Filipe de Andrade Maldonado <filipeamaldonado@gmail.com>
2019-03-18 21:44:07 +0000

    feat:add Watson Natural Language Understanding Service

--- credentials/README.md
+++ credentials/README.md
@@ -6,6 +6,21 @@ File: `algorithmia.json`
 
 ```
 {
-    "apiKey": "****************************"
+    "apiKey": "***
+}
+```
+
+## Watson Natural Language Understanding
+
+File: `watson-nlu.json`
+
+```
+{
+    "apikey": "***",
+    "iam_apikey_description": "***",
+    "iam_apikey_name": "***",
+    "iam_role_crn": "***",
+    "iam_serviceid_crn": "***",
+    "url": "https://***.watsonplatform.net/natural-language-understanding/api"
 }
 ```
--- robots/text.js
+++ robots/text.js
@@ -1,20 +1,34 @@
 const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+
 const sentenceBoundaryDetection = require('sbd')
+
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+ 
+var nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway-wdc.watsonplatform.net/natural-language-understanding/api'
+})
+
+
 
 async function robot(content){
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     function breakContentIntoSentences(content){
         content.sentences = []
 
         const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
 
-        sentences.forEach((sentences) => {
+        sentences.forEach((sentence) => {
             content.sentences.push({
-                text: sentences,
+                text: sentence,
                 keywords: [],
                 images: []
             }
@@ -31,6 +45,39 @@ async function robot(content){
         const wikipediaContent = wikipediaResponse.get()
 
         content.sourceContentOriginal = wikipediaContent.content
+    }
+
+    async function fetchKeywordsOfAllSentences(content){
+        for (const sentence of content.sentences){
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence){
+        return new Promise((resolve, reject) => {
+            nlu.analyze(
+                {
+                    text: sentence,
+                    features: {
+                        keywords: {}
+                    }
+                }, (error, response) => {
+                    if (error) {
+                        throw error
+                    }
+                    const keywords = response.keywords.map((keyword) =>{
+                        return keyword.text
+                    }
+                    )
+                    resolve(keywords)
+                }
+            )
+        }
+        )
+    }
+
+    function limitMaximumSentences(content){
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
     }
 
     function sanitizeContent(content){
